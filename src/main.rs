88cd1c648e784cdc0fe9d29cd2bc92fd6d50c677
Justin Moon <mail@justinmoon.com>
2022-05-21 20:33:31 -0700

    Handle Segwit descriptors

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,31 @@ use std::sync::mpsc::channel;
 use std::thread::spawn;
 use std::time::Instant;
 
-use bitcoin::Network;
+use bitcoin::{
+    secp256k1::{Secp256k1, Verification},
+    Network,
+};
 use miniscript::{Descriptor, DescriptorPublicKey, DescriptorTrait};
+
+fn address_prefix_length<T: Verification>(
+    desc: &Descriptor<DescriptorPublicKey>,
+    secp: &Secp256k1<T>,
+) -> usize {
+    let address = desc
+        .derived_descriptor(&secp, 1)
+        .expect("Error: Failed to derive child descriptor")
+        .address(Network::Bitcoin)
+        .expect("Error: Failed to derive address")
+        .to_string();
+
+    if address.chars().nth(0).unwrap().is_numeric() {
+        // legacy addresses have 1 character prefix
+        return 1;
+    } else {
+        // segwit addresses have 4 character prefix
+        return 4;
+    }
+}
 
 fn main() -> Result<(), anyhow::Error> {
     let args: Vec<String> = std::env::args().collect();
@@ -14,19 +37,16 @@ fn main() -> Result<(), anyhow::Error> {
         return Ok(());
     }
 
-    let raw_desc = &args[1];
-    // p2pkh addresses start with one.
-    // eventually we'll need to figure out the prefix to the prefix according to
-    // on descriptor-by-descriptor basis
-    let prefix = format!("1{}", &args[2]); // p2pkh addresses start with one.
-                                           //
-    let desc = Descriptor::<DescriptorPublicKey>::from_str(&raw_desc)?;
+    let desc = Descriptor::<DescriptorPublicKey>::from_str(&args[1])?;
     if !desc.is_deriveable() {
         println!("Error: Not a ranged ('*' inside) descriptor, can't derive addresses");
         return Ok(());
     }
 
-    let secp = bitcoin::secp256k1::Secp256k1::verification_only();
+    let secp = Secp256k1::verification_only();
+    let prefix = &args[2]; // p2pkh addresses start with one.
+    let prefix_len = prefix.len();
+    let skipped = address_prefix_length(&desc, &secp);
     let timer = Instant::now();
     let num_threads = num_cpus::get();
     let (sender, receiver) = channel();
@@ -44,11 +64,12 @@ fn main() -> Result<(), anyhow::Error> {
                 .expect("Error: Failed to derive address")
                 .to_string();
 
-            if address.starts_with(&prefix) {
+            if address[skipped..skipped + prefix_len] == prefix {
                 sender
                     .send(address)
                     .expect("Error: Couldn't send result over channel");
             }
+
             index += num_threads;
         });
     }
@@ -56,6 +77,7 @@ fn main() -> Result<(), anyhow::Error> {
     let address = receiver
         .recv()
         .expect("Error: Couldn't receive result over channel");
+
     println!("{}", address);
     println!("Took {} seconds", timer.elapsed().as_secs());
 
