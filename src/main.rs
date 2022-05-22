b712ed6ccc2ff39b20dafa4b71950765d9964d87
Justin Moon <mail@justinmoon.com>
2022-05-21 19:01:08 -0700

    Check that we receive a ranged descriptor

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,14 @@ fn main() -> Result<(), anyhow::Error> {
     // eventually we'll need to figure out the prefix to the prefix according to
     // on descriptor-by-descriptor basis
     let prefix = format!("1{}", &args[2]); // p2pkh addresses start with one.
+                                           //
     let desc = Descriptor::<DescriptorPublicKey>::from_str(&raw_desc)?;
-    let secp = bitcoin::secp256k1::Secp256k1::verification_only();
+    if !desc.is_deriveable() {
+        println!("Error: Not a ranged ('*' inside) descriptor, can't derive addresses");
+        return Ok(());
+    }
 
+    let secp = bitcoin::secp256k1::Secp256k1::verification_only();
     let mut index = 0;
     loop {
         let address = desc
